20bacce28190b76d842316e01794f8ac7a871ee3
MityakovSA <mityakovsergey@mail.ru>
2017-12-06 23:54:17 +0300

    backup

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "binary_heap.hpp"
 
-int main() {
+int main()
+{
+    BinaryHeap<int> heap1(10);
+
+    //BinaryHeap<char*>::Node n1(2, "dj");
+    /*BinaryHeap::Node n2(14, "dj");
+    BinaryHeap::Node n3(-10, "dj");
+    BinaryHeap::Node n4(15, "dj");
+    BinaryHeap::Node n5(6, "dj");
+    BinaryHeap::Node n6(0, "dj");
+    std::vector<BinaryHeap::Node> arr;
+    arr.push_back(n1);
+    arr.push_back(n2);
+    arr.push_back(n3);
+    arr.push_back(n4);
+    arr.push_back(n5);
+    arr.push_back(n6);
+    BinaryHeap heap2(arr);*/
+
     std::cout << "Lest's compare!" << std::endl;
     return 0;
 }
---
+++ include/binary_heap.hpp
@@ -0,0 +1,108 @@
+#ifndef HEAPSCOMPARISON_BINARY_HEAP_HPP
+#define HEAPSCOMPARISON_BINARY_HEAP_HPP
+
+#include <utility>
+#include <vector>
+#include <cmath>
+
+template <typename T>
+class BinaryHeap
+{
+public:
+    struct Node
+    {
+        int key;
+        T data;
+
+        Node(int key_, const T& data_) : key(key_), data(data_) {}
+    };
+
+    friend void merge(BinaryHeap& first, BinaryHeap& second);
+
+    BinaryHeap(const std::size_t& size);
+    BinaryHeap(const std::vector<Node>& arr);
+
+    Node extractMin();
+    void insert(const Node& node);
+    std::size_t size() const;
+
+private:
+    std::vector<Node> heap;
+
+    void buildHeap();
+    void siftDown(std::size_t i);
+    void siftUp(std::size_t i);
+};
+
+template <typename T>
+BinaryHeap<T>::BinaryHeap(const std::size_t& size) : heap(size) {}
+
+template <typename T>
+BinaryHeap<T>::BinaryHeap(const std::vector<Node>& arr) : heap(arr) { buildHeap(); }
+
+template <typename T>
+void BinaryHeap<T>::buildHeap()
+{
+    for (auto i = std::floor(heap.size() / 2 - 1); i >=0; i--)
+        siftDown(i);
+}
+
+template <typename T>
+void BinaryHeap<T>::siftDown(std::size_t i)
+{
+    while (2 * i + 1 < heap.size())
+    {
+        std::size_t left = 2 * i + 1;
+        std::size_t right = 2 * i + 2;
+        std::size_t j = left;
+        if ((right < heap.size()) && (heap.at(right).key < heap.at(left).key))
+            j = right;
+        if (heap.at(i).key <= heap.at(j).key)
+            break;
+        std::swap(heap.at(i), heap.at(j));
+        i = j;
+    }
+}
+
+template <typename T>
+void BinaryHeap<T>::siftUp(std::size_t i)
+{
+    while (heap.at(i).key < heap.at(std::floor((i - 1) / 2)).key)
+    {
+        std::swap(heap.at(i), heap.at(std::floor((i - 1) / 2)));
+        i = std::floor((i - 1) / 2);
+    }
+}
+
+template <typename T>
+typename BinaryHeap<T>::Node BinaryHeap<T>::extractMin()
+{
+    Node min = heap.at(0);
+    heap.at(0) = heap.at(heap.size() - 1);
+    heap.pop_back();
+    siftDown(0);
+    return min;
+}
+
+template <typename T>
+void BinaryHeap<T>::insert(const Node& node)
+{
+    heap.push_back(node);
+    siftUp(heap.size() - 1);
+}
+
+template <typename T>
+std::size_t BinaryHeap<T>::size() const
+{
+    return heap.size();
+}
+
+template <typename T>
+void merge(BinaryHeap<T>& first, BinaryHeap<T>& second)
+{
+    for (auto& node : second.heap)
+        first.heap.push_back(node);
+    first.buildHeap();
+}
+
+#endif //HEAPSCOMPARISON_BINARY_HEAP_HPP
